25f74e9d81b8e1ec10048280a5d7397c2df5dcad
heidiqueen1 <heidisanchez103.9@gmail.com>
2024-06-08 14:58:08 -0500

    Repositorio con prueba de playwright uno

--- tests/app.spec.js
+++ tests/app.spec.js
@@ -4,5 +4,10 @@ test('has title', async ({ page }) => {
     await page.goto('http://localhost:3000/');
   
     // Expect a title "to contain" a substring.
-    await expect(page).toHaveTitle(/Mi blog personal/);
+    await expect(page).toHaveTitle('Blog H.Sánchez');
+
+     // Expects page to have a heading with the name of Installation.
+     await expect(page.getByRole('heading', { name: 'Mi blog personal' })).toBeVisible();
   });
+
+ 
