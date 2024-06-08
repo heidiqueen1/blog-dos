3230154ffa95368be50ece4f75e9db5033b9e893
heidiqueen1 <heidisanchez103.9@gmail.com>
2024-06-08 14:38:08 -0500

    nuevo test  app test - uno

---
+++ tests/app.spec.js
@@ -0,0 +1,8 @@
+const { test, expect } = require('@playwright/test');
+
+test('has title', async ({ page }) => {
+    await page.goto('http://localhost:3000/');
+  
+    // Expect a title "to contain" a substring.
+    await expect(page).toHaveTitle(/Mi blog personal/);
+  });
